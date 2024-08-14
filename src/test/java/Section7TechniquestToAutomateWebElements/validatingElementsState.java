984f2e058a5895adeb633cb96f9e53ecfa54933c
Gtvaradze <gtvaradzei@gmail.com>
2024-08-14 12:38:42 +0400

    Validating Elements State

--- src/test/java/Section7TechniquestToAutomateWebElements/validatingElementsState.java
+++ src/test/java/Section7TechniquestToAutomateWebElements/validatingElementsState.java
@@ -1,18 +1,45 @@
 package Section7TechniquestToAutomateWebElements;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
+
+import java.time.Duration;
 
 public class validatingElementsState {
 
     @Test(description = "Validating if UI Elements are disabled or enabled with Attributes")
-    public void validatingElementsState(){
+    public void validatingElementsState() throws InterruptedException {
 
         System.setProperty("webdriver.chrome.driver","C://Program Files (x86)//WebDrivers//chromedriver.exe");
         WebDriver driver = new ChromeDriver();
-        driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+        driver.get("https://www.spicejet.com/");
+        driver.manage().window().maximize();
 
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
 
+        //Style opacity changing from null to 1 when enabling return date
+        if(wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("div[data-testid='return-date-dropdown-label-test-id']")))
+                .getAttribute("style")
+                .contains("opacity: 1")){
+            System.out.println("Button is enabled");
+        } else  {
+            System.out.println("Button is disabled");
+        }
+
+        driver.findElement(By.cssSelector("div[data-testid='round-trip-radio-button']")).click();
+
+        if(wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("div[data-testid='return-date-dropdown-label-test-id']")))
+                .getAttribute("class")
+                .contains("r-14lw9ot")){
+            System.out.println("Button is enabled");
+        } else  {
+            System.out.println("Button is disabled");
+        }
+
+        driver.quit();
     }
 }
