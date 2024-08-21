5466d699766fa6db5eb40f27cc0ce4fc7ef1b7dd
gtvaradze24@gmail.com <gtvaradzei@gmail.com>
2024-08-21 18:05:38 +0400

    working on Section 11

--- src/test/java/Section11RealTimeExercise/realTimeExercise.java
+++ src/test/java/Section11RealTimeExercise/realTimeExercise.java
@@ -1,11 +1,17 @@
 package Section11RealTimeExercise;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class realTimeExercise {
 
     @Test
     public void test(){
+        System.setProperty("webdriver.chrome.driver","C://Program Files (x86)//WebDrivers//chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://the-internet.herokuapp.com/");
 
     }
 }
